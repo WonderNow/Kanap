c14f558a1d8a874e5cfa3eddae9a3943b5de79ac
Tom Gasche <tomgasche@hotmail.com>
2022-09-07 18:06:33 +0200

    Ajout de commentaires dans product.js

--- front/js/product.js
+++ front/js/product.js
@@ -1,37 +1,38 @@
+let data;
+let id;
+
 // Fonction de complétion des balises HTML par les données de l'API
 let productsFetch = async function () {
-    fetch("http://localhost:3000/api/products/")
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data);
-  
-      // get data image
-      let img = document.getElementByClass(".item__img");
-      img[0].innerHTML = "<img src=" + data.imageUrl + " alt=" + data.altTxt + "></img>";
-      console.log(img);
-      console.log(data.imageURL);
+  fetch("http://localhost:3000/api/products/")
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
 
-      for (i = 0; i < data.length; i++) {
-       
-        // data.name and title
-        let name = document.getElementById("title");
-        name.innerHTML = `${data.name}`;
-        let title = document.querySelector("title");
-        title.innerHTML = `${data.name}`;
-        // price
-        let price = document.getElementById("price");
-        price.innerHTML = `${data.price}`;
-        // description
-        let description = document.getElementById("description");
-        description.innerHTML = `${data.description}`;
-        // colors
-        let color = document.getElementById("colors");
-        for (i = 0; i < data.colors.length; i++) {
-          color.innerHTML += `<option value="${data.colors[i]}">${data.colors[i]}</option>`;
-        }
-      }
+    // Image du canapé
+    let img = document.getElementById("item__img");
+    img[0].innerHTML = "<img src=" + data.imageUrl + " alt=" + data.altTxt + "></img>";
+    console.log(img);
+    console.log(data.imageURL);
 
-      });
+    // Nom du canapé
+    let name = document.getElementById("title");
+    name.innerHTML = `${data.name}`;
+
+    // Prix du canapé
+    let price = document.getElementById("price");
+    price.innerHTML = `${data.price}`;
+
+    // Description du canapé
+    let description = document.getElementById("description");
+    description.innerHTML = `${data.description}`;
+
+    // Couleur(s) du canapé
+    let color = document.getElementById("colors");
+    for (i = 0; i < data.colors.length; i++) {
+      color.innerHTML += `<option value="${data.colors[i]}">${data.colors[i]}</option>`;
+    }
+
+    });
   };
 
 // Exécution de productsFetch
